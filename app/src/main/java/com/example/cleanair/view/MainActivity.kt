f3b47360dd06adae805ce248d350017f705faa80
cristian <constantincristian05@icloud.com>
2023-12-20 00:04:51 +0200

    Added reset button and feature layers for two options

--- app/src/main/java/com/example/cleanair/view/MainActivity.kt
+++ app/src/main/java/com/example/cleanair/view/MainActivity.kt
@@ -2,19 +2,25 @@ package com.example.cleanair.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.data.ServiceFeatureTable
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.layers.FeatureLayer
 import com.example.cleanair.CleanAirApplication
+import com.example.cleanair.R
 import com.example.cleanair.databinding.ActivityMainBinding
 import com.example.cleanair.presenter.MainPresenter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,6 +54,53 @@ class MainActivity : AppCompatActivity() {
         binding.mapView.map?.apply {
             initialViewpoint = Viewpoint(29.7604, -95.3698, 1000000.0)
         }
+
+        binding.resetViewpointButton.setOnClickListener {
+            binding.mapView.setViewpoint(Viewpoint(29.7604, -95.3698, 1000000.0))
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.air_quality -> {
+                    // Respond to navigation item 1 click
+                    showToast("AirQuality")
+                    true
+                }
+
+                R.id.pm2_5 -> {
+                    // Respond to navigation item 2 click
+                    showToast("PM_2.5")
+                    val serviceFeatureTable =
+                        ServiceFeatureTable("https://services.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/USA_PM25_1998_to_2016/FeatureServer")
+                    // create a feature layer with the feature table
+                    val featureLayer = FeatureLayer.createWithFeatureTable(serviceFeatureTable)
+
+                    binding.mapView.map!!.operationalLayers.clear()
+                    binding.mapView.map!!.operationalLayers.add(featureLayer)
+                    true
+                }
+
+                R.id.national_parks -> {
+                    // Respond to navigation item 3 click
+                    showToast("National Parks")
+                    val serviceFeatureTable =
+                        ServiceFeatureTable("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_Parks/FeatureServer/0")
+                    // create a feature layer with the feature table
+                    val featureLayer = FeatureLayer.createWithFeatureTable(serviceFeatureTable)
+
+                    binding.mapView.map!!.operationalLayers.clear()
+                    binding.mapView.map!!.operationalLayers.add(featureLayer)
+                    true
+                }
+
+                R.id.pollution -> {
+                    // Respond to navigation item 4 click
+                    showToast("Pollution")
+                    true
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 
     public override fun onStart() {
@@ -62,5 +115,9 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginScreenActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
